eeea90a7000b25eb0be4cf89f0ae4aace7b75631
liangsqrt <liangsqrt@163.com>
2020-01-26 15:34:51 +0800

    过年在家修改

--- zhiyuan/apps.py
+++ zhiyuan/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ZhiyuanConfig(AppConfig):
     name = 'zhiyuan'
+    verbose_name = "找大学模块数据"
--- zhiyuan/views.py
+++ zhiyuan/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import SchoolFilterSerializer
 from .models import SchoolOverViewItem
+from rest_framework.response import Response
+import json
 # Create your views here.
 
 
@@ -13,5 +15,16 @@ class SchooleFilter(APIView):
         print(request)
 
     def get(self, request, *args, **kwargs):
+        province_list = self.queryset.values("province").distinct()
+        is_985 = [0,1]
+        school_types_list = self.queryset.values("school_types").distinct()
+        tags_list = self.queryset.values("tags").distinct()
+        result = {
+            "province": [x["province"] for x in province_list],
+            "is_985": is_985,
+            "school_types": [x["school_types"] for x in school_types_list],
+            "tags": [x["tags"] for x in tags_list]
+        }
         self.queryset
         print(request)
+        return Response(data=json.dumps(result))
